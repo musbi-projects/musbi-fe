af7d3d26986355208e91776a70e0291fda1648b0
zubetcha <zuhye5@gmail.com>
2023-01-09 23:29:11 +0900

    setting: _app.tsx 세팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+
+import { useState } from 'react';
+import { QueryClient, QueryClientProvider, Hydrate } from '@tanstack/react-query';
+import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from 'styled-components';
+
+import { theme, GlobalStyle } from '@/styles';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <RecoilRoot>
+          <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </RecoilRoot>
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
---
+++ src/styles/globalStyle.tsx
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
+}
+
+ul, ol {
+  list-style-type: none;
+}
+
+body {
+  color: red;
+}
+
+`;
