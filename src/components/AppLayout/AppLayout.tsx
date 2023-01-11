8dd969539cbb8b2282befedcb713bf08218320ca
limchansoo <cslim0527@gmail.com>
2023-01-11 23:44:12 +0900

    Feat: AppLayout 컴포넌트 작업

---
+++ src/components/AppLayout/AppLayout.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+
+export interface AppLayoutProps {
+  children: React.ReactNode | React.ReactNode[];
+}
+
+const AppLayout = ({ children }: AppLayoutProps) => {
+  return (
+    <StyledLayout>
+      <StyledContainer>{children}</StyledContainer>
+    </StyledLayout>
+  );
+};
+
+export default AppLayout;
+
+const StyledLayout = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
+
+const StyledContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  max-width: 1920px;
+  margin: 0 auto;
+`;
