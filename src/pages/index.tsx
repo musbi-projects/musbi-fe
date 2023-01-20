cb364189082ff4d9193a83f58eb130ba31988986
limchansoo <cslim0527@gmail.com>
2023-01-20 22:20:43 +0900

    Feat: Input 컴포넌트 파일생성, AppLayout 공통 영역으로 이동 (_app.tsx)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,8 @@
 import React from "react";
-import AppLayout from "@/components/AppLayout/AppLayout";
-import Header from "@/components/Header";
-import styled from "styled-components";
 import { Inter } from "@next/font/google";
-import ToolBox from "@/components/ToolBox";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const SampleLeft = () => {
-  return <h1>musbi</h1>;
-};
-
-const SampleRight = () => {
-  return (
-    <div>
-      <span>Menu 1</span>
-      <span>Menu 2</span>
-    </div>
-  );
-};
-
 export default function Home() {
-  return (
-    <AppLayout>
-      <Header left={<SampleLeft />} right={<SampleRight />} />
-      <ToolBox />
-      <StyledMainContainer>asdsadsadsadd</StyledMainContainer>
-    </AppLayout>
-  );
+  return <div>Home</div>;
 }
-
-const StyledMainContainer = styled.main``;
