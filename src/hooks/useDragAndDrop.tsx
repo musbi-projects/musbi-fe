efe1e4f60c380242ddce02c589f50c4ef9ec33c5
zubetcha <zuhye5@gmail.com>
2023-02-12 17:54:42 +0900

    Refactor: prettier singlequote 수정 및 useDragAndDrop DOM 참조 방식 useRef로 수정

--- src/hooks/useDragAndDrop.tsx
+++ src/hooks/useDragAndDrop.tsx
@@ -1,39 +1,40 @@
-import React, { useState, useEffect, useCallback, DragEvent } from "react";
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 
 interface Params {
   onChange: (e: globalThis.DragEvent) => void;
 }
 
 export const useDragAndDrop = <T extends HTMLElement>({ onChange }: Params) => {
-  const [isDragging, setIsDragging] = useState(false);
-  const [node, setNode] = useState<T | null>(null);
+  const dragRef = useRef<T>(null);
 
-  const dragRef = useCallback((node: T) => {
-    if (node !== null) {
-      setNode(node);
-    }
-  }, []);
+  const [isDragging, setIsDragging] = useState(false);
 
   const handleDragIn = useCallback((e: globalThis.DragEvent): void => {
     e.preventDefault();
     e.stopPropagation();
   }, []);
 
-  const handleDragOut = useCallback((e: globalThis.DragEvent): void => {
-    e.preventDefault();
-    e.stopPropagation();
+  const handleDragOut = useCallback(
+    (e: globalThis.DragEvent): void => {
+      e.preventDefault();
+      e.stopPropagation();
 
-    setIsDragging(false);
-  }, []);
+      setIsDragging(false);
+    },
+    [setIsDragging],
+  );
 
-  const handleDragOver = useCallback((e: globalThis.DragEvent): void => {
-    e.preventDefault();
-    e.stopPropagation();
+  const handleDragOver = useCallback(
+    (e: globalThis.DragEvent): void => {
+      e.preventDefault();
+      e.stopPropagation();
 
-    if (e.dataTransfer!.files) {
-      setIsDragging(true);
-    }
-  }, []);
+      if (e.dataTransfer!.files) {
+        setIsDragging(true);
+      }
+    },
+    [setIsDragging],
+  );
 
   const handleDrop = useCallback(
     (e: globalThis.DragEvent): void => {
@@ -43,36 +44,32 @@ export const useDragAndDrop = <T extends HTMLElement>({ onChange }: Params) => {
       onChange(e);
       setIsDragging(false);
     },
-    [onChange],
+    [onChange, setIsDragging],
   );
 
-  const initDragEvents = useCallback((): void => {
-    // 앞서 말했던 4개의 이벤트에 Listener를 등록합니다. (마운트 될때)
-
-    if (node) {
-      node.addEventListener("dragenter", handleDragIn);
-      node.addEventListener("dragleave", handleDragOut);
-      node.addEventListener("dragover", handleDragOver);
-      node.addEventListener("drop", handleDrop);
+  const setDragEvents = useCallback((): void => {
+    if (dragRef.current) {
+      dragRef.current.addEventListener('dragenter', handleDragIn);
+      dragRef.current.addEventListener('dragleave', handleDragOut);
+      dragRef.current.addEventListener('dragover', handleDragOver);
+      dragRef.current.addEventListener('drop', handleDrop);
     }
-  }, [handleDragIn, handleDragOut, handleDragOver, handleDrop, node]);
+  }, [handleDragIn, handleDragOut, handleDragOver, handleDrop, dragRef]);
 
-  const resetDragEvents = useCallback((): void => {
-    // 앞서 말했던 4개의 이벤트에 Listener를 삭제합니다. (언마운트 될때)
-
-    if (node) {
-      node.removeEventListener("dragenter", handleDragIn);
-      node.removeEventListener("dragleave", handleDragOut);
-      node.removeEventListener("dragover", handleDragOver);
-      node.removeEventListener("drop", handleDrop);
+  const removeDragEvents = useCallback((): void => {
+    if (dragRef.current) {
+      dragRef.current.removeEventListener('dragenter', handleDragIn);
+      dragRef.current.removeEventListener('dragleave', handleDragOut);
+      dragRef.current.removeEventListener('dragover', handleDragOver);
+      dragRef.current.removeEventListener('drop', handleDrop);
     }
-  }, [handleDragIn, handleDragOut, handleDragOver, handleDrop, node]);
+  }, [handleDragIn, handleDragOut, handleDragOver, handleDrop, dragRef]);
 
   useEffect(() => {
-    initDragEvents();
+    setDragEvents();
 
-    return () => resetDragEvents();
-  }, [initDragEvents, resetDragEvents]);
+    return () => removeDragEvents();
+  }, [setDragEvents, removeDragEvents]);
 
   return {
     dragRef,
