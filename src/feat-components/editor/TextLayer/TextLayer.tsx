53c15c5f710894a759afbc67e7a5f79c5833d04c
limchansoo <cslim0527@gmail.com>
2023-03-02 23:21:50 +0900

    Feat: react-moveable 적용
    - 레이어 초기 렌더링 시 레이어 테두리 포지션 안맞는 버그 있음
    - 리사이징 시 버벅임 있음

--- src/feat-components/editor/TextLayer/TextLayer.tsx
+++ src/feat-components/editor/TextLayer/TextLayer.tsx
@@ -1,7 +1,6 @@
 import React, { memo, useCallback, useEffect, useState, FocusEvent, useRef, useLayoutEffect, useMemo } from 'react';
 import ContentEditable, { ContentEditableEvent } from 'react-contenteditable';
-import sanitizeHtml from 'sanitize-html';
-import Draggable, { DraggableData, DraggableEvent } from 'react-draggable';
+import Moveable, { OnDrag, OnResize } from 'react-moveable';
 import styled, { css } from 'styled-components';
 import { useOutsideClick } from '@/hooks';
 import { useSetEditorViewState } from '@/recoil/editorView';
@@ -10,6 +9,11 @@ import { useCanvasValue } from '@/recoil/canvas';
 interface LayerPosition {
   top: number;
   left: number;
+}
+
+interface LayerSize {
+  width: 'auto' | number;
+  height: 'auto' | number;
 }
 
 interface TextLayerProps {
@@ -31,9 +35,11 @@ const TextLayer = (props: TextLayerProps) => {
   const [clicked, setClicked] = useState<boolean>(false);
   const [isDragging, setIsDragging] = useState<boolean>(false);
   const [isDisabled, setIsDisabled] = useState(true);
-  const [layerPosition, setLayerPosition] = useState<LayerPosition>({ top: 0, left: 0 });
   const setEditorViewState = useSetEditorViewState();
   const { currentCanvas } = useCanvasValue();
+  const [frame, setFrame] = useState<Record<string, any>>({ width: 'auto', height: 'auto', top: 0, left: 0 });
+
+  console.log('[frame]', frame);
 
   // TODO: 현재 TextLayer 내용 한정 update 추후 fontFamily, name 등등 상태변경도 필요
   const updateEditorViewState = (text: string) => {
@@ -91,29 +97,47 @@ const TextLayer = (props: TextLayerProps) => {
     if (!isDisabled) return false;
   }, [isDisabled]);
 
-  const handleDraggingLayer = useCallback((e: DraggableEvent, data: DraggableData) => {
-    // console.log('[handleDrag]', data);
-    setIsDragging(true);
-  }, []);
+  const handleDraggingLayer = useCallback(
+    ({ left, top }: OnDrag) => {
+      setFrame({ ...frame, top, left });
+    },
+    [frame],
+  );
 
   const handleStopDragLayer = useCallback(() => {
     setIsDragging(false);
   }, []);
 
+  const handleResizeLayer = useCallback(
+    ({ target, width, height, transform }: OnResize) => {
+      console.log('height', height);
+      setFrame({ ...frame, width, height });
+      target.style.transform = transform;
+    },
+    [frame],
+  );
+
   const handleClickLayer = useCallback(() => {
     console.log('[handleClickLayer]');
   }, []);
 
-  // 초기 중앙 포지셔닝을 위해 레이어 사이즈를 계산해
-  const initLayerSize = useCallback(() => {
+  const getLayerStartPosition = useCallback(() => {
+    if (!layerRef.current) return { top: 0, left: 0 };
     const layerRect = layerRef.current!.getBoundingClientRect();
     const left = center?.left - layerRect.width / 2;
-    setLayerPosition({ ...center, left });
+    return { top: center.top, left };
+  }, [layerRef.current]);
+
+  // 초기 중앙 포지셔닝을 위해 레이어 사이즈를 계산해
+  const initLayer = useCallback(() => {
+    console.log('[initLayer]');
+    const { top, left } = getLayerStartPosition();
+    setFrame({ ...frame, top, left });
   }, [layerRef.current]);
 
   useLayoutEffect(() => {
     if (!layerRef.current) return;
-    initLayerSize();
+    initLayer();
   }, [layerRef.current]);
 
   useEffect(() => {
@@ -121,15 +145,25 @@ const TextLayer = (props: TextLayerProps) => {
   }, [initialContent]);
 
   return (
-    <Draggable
-      onStart={handleStartDragLayer}
-      onDrag={handleDraggingLayer}
-      onStop={handleStopDragLayer}
-      nodeRef={layerRef}
-    >
+    <>
+      <Moveable
+        origin={false}
+        draggable={true}
+        resizable={true}
+        throttleDrag={1}
+        throttleResize={1}
+        throttleScale={0.01}
+        throttleRotate={0.2}
+        pinchThreshold={20}
+        target={layerRef.current}
+        onDrag={handleDraggingLayer}
+        onDragStart={handleStartDragLayer}
+        onDragEnd={handleStopDragLayer}
+        onResize={handleResizeLayer}
+      />
       <StyledTextLayer
+        frame={frame}
         clicked={clicked.toString()}
-        center={layerPosition}
         disabled={isDisabled}
         fontFamily={fontFamily}
         fontWeight={fontWeight}
@@ -141,7 +175,7 @@ const TextLayer = (props: TextLayerProps) => {
         innerRef={layerRef}
         tagName='div'
       />
-    </Draggable>
+    </>
   );
 };
 
@@ -152,18 +186,19 @@ const StyledTextLayer = styled(ContentEditable)<{
   fontWeight: number;
   disabled: boolean;
   clicked: string;
-  center: { top: number; left: number };
+  frame: Record<string, any>;
 }>`
-  ${({ theme, fontFamily, fontWeight, disabled, clicked, center }) => css`
+  ${({ theme, fontFamily, fontWeight, disabled, clicked, frame }) => css`
     outline: none;
     font-family: ${fontFamily};
     font-weight: ${fontWeight ? fontWeight : 'normal'};
     cursor: ${disabled ? 'move' : 'initial'};
     position: absolute;
     display: inline-block;
-    top: ${center.top}px;
-    left: ${center.left}px;
-
+    top: ${frame?.top}px;
+    left: ${frame?.left}px;
+    width: ${typeof frame.width === 'string' ? frame.width : `${frame.width}px`};
+    height: ${typeof frame.height === 'string' ? frame.height : `${frame.height}px`};
 
     &:hover,
     &:focus {
